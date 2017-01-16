baec2d01fd1b5915d5f12510159bbacd0352805e
lkuffo <lkuffo@espol.edu.ec>
2017-01-16 00:08:05 -0500

    Combo box de Conferencias llenado con JS + Servlet

---
+++ PracticaServlet/build/generated/src/org/apache/jsp/asistentes_jsp.java
@@ -0,0 +1,271 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+import java.text.SimpleDateFormat;
+import modelos.Conferencia;
+import java.util.List;
+
+public final class asistentes_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html lang=\"en\">\n");
+      out.write("  <head>\n");
+      out.write("    <meta charset=\"utf-8\">\n");
+      out.write("    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n");
+      out.write("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n");
+      out.write("    <title>Conferencias</title>\n");
+      out.write("\n");
+      out.write("    <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\" integrity=\"sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7\" crossorigin=\"anonymous\">\n");
+      out.write("    <link rel=\"stylesheet\" href=\"stylesheet/styles.css\">\n");
+      out.write("    <link rel=\"stylesheet\" type=\"text/css\" href=\"https://cdn.datatables.net/v/bs/dt-1.10.12/datatables.min.css\"/>\n");
+      out.write("  </head>\n");
+      out.write("\n");
+      out.write("  <body>\n");
+      out.write("    <nav class=\"navbar navbar-inverse navbar-static-top\">\n");
+      out.write("      <div class=\"container\">\n");
+      out.write("        <div class=\"navbar-header\">\n");
+      out.write("          <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#navbar\" aria-expanded=\"false\" aria-controls=\"navbar\">\n");
+      out.write("            <span class=\"sr-only\">Toggle navigation</span>\n");
+      out.write("            <span class=\"icon-bar\"></span>\n");
+      out.write("            <span class=\"icon-bar\"></span>\n");
+      out.write("            <span class=\"icon-bar\"></span>\n");
+      out.write("          </button>\n");
+      out.write("          <a class=\"navbar-brand\" href=\"#\">AdmProyectos</a>\n");
+      out.write("        </div>\n");
+      out.write("        <div id=\"navbar\" class=\"collapse navbar-collapse\">\n");
+      out.write("          <ul class=\"nav navbar-nav\">\n");
+      out.write("            <li ><a href=\"index.jsp\">Inicio</a></li>\n");
+      out.write("            <li ><a href=\"Conferencia\">Conferencias</a></li>\n");
+      out.write("            <li class=\"active\"><a href=\"#\">Asistentes</a></li>\n");
+      out.write("          </ul>\n");
+      out.write("          \n");
+      out.write("        </div><!--/.nav-collapse -->\n");
+      out.write("      </div>\n");
+      out.write("    </nav>\n");
+      out.write("    <div class=\"container usuarios\">\n");
+      out.write("      <h2>Registro de Asistentes</h2>\n");
+      out.write("      <button type=\"button\" class=\"btn btn-primary agregar\" data-toggle=\"modal\" data-target=\"#modalUsuarios\">Agregar Asistentes</button>\n");
+      out.write("      <table id=\"tablaUsuarios\" class=\"table table-bordered table-striped\">\n");
+      out.write("        <thead>\n");
+      out.write("          <tr>\n");
+      out.write("            <th>Cedula</th>\n");
+      out.write("            <th>Nombre</th>\n");
+      out.write("            <th>Apellido</th>\n");
+      out.write("            <th>Conferencia</th>\n");
+      out.write("            <th>Correo</th>\n");
+      out.write("            \n");
+      out.write("            <th></th>\n");
+      out.write("            <th></th>\n");
+      out.write("          </tr>\n");
+      out.write("        </thead>\n");
+      out.write("        <tbody id =\"asistentes\">\n");
+      out.write("        \n");
+      out.write("        </tbody>\n");
+      out.write("      </table>\n");
+      out.write("    </div>\n");
+      out.write("\n");
+      out.write("    <!-- Modal -->\n");
+      out.write("    <div class=\"modal fade\" id=\"modalUsuarios\" tabindex=\"-1\" role=\"dialog\">\n");
+      out.write("      <div class=\"modal-dialog\" role=\"document\">\n");
+      out.write("        <div class=\"modal-content\">\n");
+      out.write("          <form action=\"Conferencia\" method=\"post\">\n");
+      out.write("            <div class=\"modal-header\">\n");
+      out.write("              <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>\n");
+      out.write("              <h4 class=\"modal-title\">Ingresar Asistente</h4>\n");
+      out.write("            </div>\n");
+      out.write("            <div class=\"modal-body\">\n");
+      out.write("                <div class=\"form-group\">\n");
+      out.write("                  <input type=\"text\" class=\"form-control\" id=\"inputCedula\" name=\"cedula\" placeholder=\"Ingrese su cedula\">\n");
+      out.write("                </div>\n");
+      out.write("                <div class=\"form-group\">\n");
+      out.write("                  <input type=\"text\" class=\"form-control\" id=\"inputNombre\" name=\"nombre\" placeholder=\"Ingrese su nombre\">\n");
+      out.write("                </div>\n");
+      out.write("                <div class=\"form-group\">\n");
+      out.write("                  <input type=\"text\" class=\"form-control\" id=\"inputApellido\" name=\"apellido\" placeholder=\"Ingrese su apellido\">\n");
+      out.write("                </div>\n");
+      out.write("                <div class=\"form-group\">\n");
+      out.write("                  <input type=\"date\" class=\"form-control\" id=\"inputConferencia\" name=\"Conferencia\" >\n");
+      out.write("                </div>\n");
+      out.write("                <div class=\"form-group\">\n");
+      out.write("                    <textarea class=\"text\"  id=\"inputCorreo\" name=\"correo\" placeholder=\"Ingrese su correo\"></textarea>\n");
+      out.write("                </div>\n");
+      out.write("                <div class=\"form-group\">\n");
+      out.write("                  <input type=\"hidden\" class=\"form-control\" id=\"action\" name=\"action\" value=\"agregar\">\n");
+      out.write("\n");
+      out.write("                </div>\n");
+      out.write("                <div class=\"form-group\">\n");
+      out.write("                  <input type=\"hidden\" class=\"form-control\" id=\"id\" name=\"id\" value=\"\">\n");
+      out.write("\n");
+      out.write("                </div>\n");
+      out.write("            </div>\n");
+      out.write("            <div class=\"modal-footer\">\n");
+      out.write("              <button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Cancelar</button>\n");
+      out.write("              <button type=\"submit\" class=\"btn btn-primary\">Ingresar</button>\n");
+      out.write("            </div>\n");
+      out.write("          </form>\n");
+      out.write("        </div>\n");
+      out.write("      </div>\n");
+      out.write("    </div>\n");
+      out.write("\n");
+      out.write("    <div class=\"modal fade\" id=\"modalEliminar\" tabindex=\"-1\" role=\"dialog\">\n");
+      out.write("      <div class=\"modal-dialog\" role=\"document\">\n");
+      out.write("        <div class=\"modal-content\">\n");
+      out.write("          <form action=\"Conferencia\" method=\"post\">\n");
+      out.write("            <div class=\"modal-header\">\n");
+      out.write("              <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>\n");
+      out.write("              <h4 class=\"modal-title\">Eliminar</h4>\n");
+      out.write("            </div>\n");
+      out.write("            <div class=\"modal-body\">\n");
+      out.write("                <div class=\"form-group\">\n");
+      out.write("                    <label>Seguro desea eliminar el registro??</label>\n");
+      out.write("                    <div class=\"form-group\">\n");
+      out.write("                    <input type=\"hidden\" class=\"form-control id\" id=\"idEliminar\" name=\"id\" value=\"\">\n");
+      out.write("                    <input type=\"hidden\" class=\"form-control\"  name=\"action\" value=\"eliminar\">\n");
+      out.write("                </div>\n");
+      out.write("                </div>\n");
+      out.write("                \n");
+      out.write("                </div>\n");
+      out.write("            \n");
+      out.write("            <div class=\"modal-footer\">\n");
+      out.write("                \n");
+      out.write("              <button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">No</button>\n");
+      out.write("              <button type=\"submit\" class=\"btn btn-primary\">SI</button>\n");
+      out.write("            </div>\n");
+      out.write("          </form>\n");
+      out.write("        </div>\n");
+      out.write("      </div>\n");
+      out.write("    </div>\n");
+      out.write("    \n");
+      out.write("    \n");
+      out.write("    <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\"></script>\n");
+      out.write("    <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js\" integrity=\"sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS\" crossorigin=\"anonymous\"></script>\n");
+      out.write("    <script src=\"javascript/conferencia.js\"></script>\n");
+      out.write("    <script type=\"text/javascript\" src=\"https://cdn.datatables.net/v/bs/dt-1.10.12/datatables.min.js\"></script>\n");
+      out.write("\n");
+      out.write("    <script>\n");
+      out.write("        $(document).ready(function() {\n");
+      out.write("            $('#tablaUsuarios').DataTable({\n");
+      out.write("                \"language\": {\n");
+      out.write("                    url: 'i8/dt-spanish.json'\n");
+      out.write("                },\n");
+      out.write("                \"aoColumnDefs\": [\n");
+      out.write("                    { 'bSortable': false, 'aTargets': [ 4, 5 ] }\n");
+      out.write("                 ]\n");
+      out.write("            });    \n");
+      out.write("            cargarAsistentes();\n");
+      out.write("            cargarConferencias();\n");
+      out.write("        });\n");
+      out.write("        \n");
+      out.write("        function cargarConferencias(){\n");
+      out.write("            var conferencias = [];\n");
+      out.write("            $.ajax({  \n");
+      out.write("              type: \"GET\",  \n");
+      out.write("              url: \"./Conferencias\",  \n");
+      out.write("              data: \"\",  \n");
+      out.write("              success: function(response){\n");
+      out.write("                  var obj = JSON.parse(response);\n");
+      out.write("                  var id = obj.id;\n");
+      out.write("                  var nombre = obj.nombre;\n");
+      out.write("                  var desc = obj.descripcion;\n");
+      out.write("                  var fecha = obj.fecha;\n");
+      out.write("                  conferencias.push(id);\n");
+      out.write("              }}); \n");
+      out.write("\n");
+      out.write("              var cadena = '<div class=\"form-group\"><select id=\"inputConferencia\" class=\"form-control\">';\n");
+      out.write("              for (var i=0; i < conferencias.length; i++){\n");
+      out.write("                  var agregar = \"<option>\"+conferencias[i]+\"</option>\";\n");
+      out.write("                  cadena += agregar;\n");
+      out.write("              }\n");
+      out.write("              cadena += \"</select></div>\";\n");
+      out.write("              $(\".modal-body\").append(cadena);           \n");
+      out.write("        }\n");
+      out.write("        \n");
+      out.write("        function cargarAsistentes(){\n");
+      out.write("            $.ajax({  \n");
+      out.write("              type: \"GET\",  \n");
+      out.write("              url: \"./AsistentesServlet\",  \n");
+      out.write("              data: \"\",  \n");
+      out.write("              success: function(response){\n");
+      out.write("                  obj = JSON.parse(response);\n");
+      out.write("                  \n");
+      out.write("              }                \n");
+      out.write("            });              \n");
+      out.write("        }\n");
+      out.write("        \n");
+      out.write("        function agregarAsistente(){\n");
+      out.write("            data = {\"cedula\": $(\"#inputCedula\").val(), \"nombre\": $(\"#inputNombre\").val(), \"apellido\": $(\"#inputApellido\").val(), \"correo\": $(\"#inputCorreo\").val(), \"conferencia\": $(\"#inputConferencia\").val()};\n");
+      out.write("            $.ajax({  \n");
+      out.write("              type: \"POST\",  \n");
+      out.write("              url: \"./AsistentesRegistrar\",  \n");
+      out.write("              data: data,  \n");
+      out.write("              success: function(){}              \n");
+      out.write("            });\n");
+      out.write("        }\n");
+      out.write("        \n");
+      out.write("        function eliminarAsistente(){\n");
+      out.write("            data = {\"cedula\": $(\"#inputCedula\").val()};\n");
+      out.write("            $.ajax({  \n");
+      out.write("              type: \"POST\",  \n");
+      out.write("              url: \"./AsistentesEliminar\",  \n");
+      out.write("              data: data,  \n");
+      out.write("              success: function(){}                \n");
+      out.write("            });                 \n");
+      out.write("        }        \n");
+      out.write("    </script>\n");
+      out.write("  </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
